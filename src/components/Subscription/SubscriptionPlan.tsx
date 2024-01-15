8252a18bea6211c5595ac1dd1c93086b7b44918d
karan <karan.tompe@ayanworks.com>
2024-01-15 19:26:39 +0530

    feat: added member table
    Signed-off-by: karan <karan.tompe@ayanworks.com>

--- src/components/Subscription/SubscriptionPlan.tsx
+++ src/components/Subscription/SubscriptionPlan.tsx
@@ -1,7 +1,10 @@
 'use client';
 
-import { Table } from 'flowbite-react';
 import React, { useState } from 'react';
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+import BreadCrumbs from '../BreadCrumbs';
+
 const SubscriptionPlan = () => {
 	const [isSecondTableOpen, setIsSecondTableOpen] = useState(false);
 
@@ -56,215 +59,212 @@ const SubscriptionPlan = () => {
 			endDate: '29/02/2024',
 		},
 	];
+
+	const memberHeader = [
+		{ columnName: 'Organization Name' },
+		{ columnName: 'Plan ' },
+	];
+
+	const memberTableData = [
+		{
+			orgName: 'ProActive.Com ',
+			planType: 'Free',
+		},
+		{
+			orgName: 'ProStudio.Com ',
+			planType: 'Pro',
+		},
+	];
+
 	return (
-		// <div className="overflow-x-auto p-8 m-4 bg-white dark:bg-gray-500 rounded-sm">
-		// 	<Table className="rounded-none">
-		// 		<Table.Head className="rounded-none">
-		// 			<Table.HeadCell>Plan</Table.HeadCell>
-		// 			<Table.HeadCell>Validity</Table.HeadCell>
-		// 			<Table.HeadCell>Start Date</Table.HeadCell>
-		// 			<Table.HeadCell>End Date</Table.HeadCell>
-		// 			<Table.HeadCell>Action</Table.HeadCell>
-		// 		</Table.Head>
-		// 		<Table.Body className="divide-y">
-		// 			<Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
-		// 				<Table.Cell
-		// 					onClick={() => {
-		// 						handleSecondTableClick();
-		// 					}}
-		// 					className="whitespace-nowrap font-medium text-gray-900 dark:text-white flex items-center"
-		// 				>
-		// 					<span>
-		// 						{isSecondTableOpen ? (
-		// 							<svg
-		// 								xmlns="http://www.w3.org/2000/svg"
-		// 								width="12"
-		// 								height="7"
-		// 								viewBox="0 0 12 7"
-		// 								fill="none"
-		// 							>
-		// 								<path
-		// 									d="M10.1643 0.725C10.4786 0.425 10.95 0.425 11.2643 0.725C11.4214 0.875 11.5 1.025 11.5 1.25C11.5 1.475 11.4214 1.625 11.2643 1.775L6.55 6.275C6.23571 6.575 5.76429 6.575 5.45 6.275L0.735714 1.775C0.421429 1.475 0.421429 1.025 0.735714 0.725C1.05 0.425 1.52143 0.425 1.83571 0.725L6 4.7L10.1643 0.725Z"
-		// 									fill="#606972"
-		// 								/>
-		// 							</svg>
-		// 						) : (
-		// 							<svg
-		// 								xmlns="http://www.w3.org/2000/svg"
-		// 								width="6"
-		// 								height="11"
-		// 								viewBox="0 0 6 11"
-		// 								fill="none"
-		// 							>
-		// 								<path
-		// 									d="M0.225 1.33571C-0.0749998 1.02143 -0.0749998 0.55 0.225 0.235715C0.375 0.0785723 0.525 0 0.75 0C0.975 0 1.125 0.0785723 1.275 0.235715L5.775 4.95C6.075 5.26429 6.075 5.73571 5.775 6.05L1.275 10.7643C0.975 11.0786 0.525 11.0786 0.225 10.7643C-0.0749998 10.45 -0.0749998 9.97857 0.225 9.66429L4.2 5.5L0.225 1.33571Z"
-		// 									fill="#606972"
-		// 								/>
-		// 							</svg>
-		// 						)}
-		// 					</span>
-		// 					<span className="pl-2">Free</span>
-		// 				</Table.Cell>
-		// 				<Table.Cell>1 month</Table.Cell>
-		// 				<Table.Cell>01/02/2024</Table.Cell>
-		// 				<Table.Cell>01/03/2024</Table.Cell>
-		// 				<Table.Cell>
-		// 					<a
-		// 						href="#"
-		// 						className="font-medium text-cyan-600 hover:underline dark:text-cyan-500"
-		// 					>
-		// 						Upgrade
-		// 					</a>
-		// 				</Table.Cell>
-		// 			</Table.Row>
-		// 		</Table.Body>
-		// 	</Table>
-		// 	{isSecondTableOpen && (
-		// 		<div className="m-5">
-		// 			<Table>
-		// 				<Table.Head>
-		// 					<Table.HeadCell>Features</Table.HeadCell>
-		// 					<Table.HeadCell>Total</Table.HeadCell>
-		// 					<Table.HeadCell>Used</Table.HeadCell>
-		// 					<Table.HeadCell>Remaining</Table.HeadCell>
-		// 					<Table.HeadCell>Percent Used</Table.HeadCell>
-		// 				</Table.Head>{' '}
-		// 				<Table.Body className="divide-y">
-		// 					<Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
-		// 						<Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-		// 							<span className="pl-2">Organization</span>
-		// 						</Table.Cell>
-		// 						<Table.Cell>2</Table.Cell>
-		// 						<Table.Cell>1</Table.Cell>
-		// 						<Table.Cell>1</Table.Cell>
-		// 						<Table.Cell>
-		// 							<a
-		// 								href="#"
-		// 								className="font-medium text-cyan-600 hover:underline dark:text-cyan-500"
-		// 							>
-		// 								50%
-		// 							</a>
-		// 						</Table.Cell>
-		// 					</Table.Row>
-		// 				</Table.Body>
-		// 			</Table>
-		// 		</div>
-		// 	)}
-		// </div>
-		<div className="overflow-x-auto p-8 m-4 bg-white dark:bg-gray-500 rounded-sm w-full">
-			<table className="rounded-none w-full table-auto">
-				<thead className="rounded-none bg-gray-200 p-4">
-					<tr>
-						{firstTableHeader &&
-							firstTableHeader.map((header) => {
+		<div className="p-4" id="connection_list">
+			<BreadCrumbs />
+			<div
+				className="flex items-center justify-between mb-4"
+				id="connection-list"
+			>
+				<h1 className="ml-1 mr-auto text-xl font-semibold text-gray-900 sm:text-2xl dark:text-white">
+					Connections
+				</h1>
+			</div>
+			<div className="overflow-x-auto p-3 mb-4 bg-white dark:bg-gray-500 rounded-sm w-full">
+				<table className="rounded-none w-full table-auto">
+					<thead className="rounded-none bg-gray-200 p-4">
+						<tr className="">
+							{firstTableHeader &&
+								firstTableHeader.map((header) => {
+									return (
+										<th className="w-1/5 md:w-1/4 lg:w-1/5 text-center m-4 p-2">
+											{header}
+										</th>
+									);
+								})}
+						</tr>
+					</thead>
+					<tbody className="divide-y w-full">
+						{subscriptionDetails &&
+							subscriptionDetails.map((subscription) => {
 								return (
-									<th className="w-1/5 md:w-1/4 lg:w-1/5  text-center">
-										{header}
-									</th>
+									<tr className="bg-white dark:border-gray-700 dark:bg-gray-800 m-4 p-2">
+										<td
+											onClick={() => handleSecondTableClick()}
+											className="whitespace-nowrap font-medium text-gray-900 dark:text-white w-1/5 md:w-1/4 lg:w-1/5 p-2 text-center"
+										>
+											<div className="flex items-center">
+												<span>
+													{isSecondTableOpen ? (
+														<svg
+															className="w-[14px] h-[14px] text-gray-800 dark:text-white"
+															aria-hidden="true"
+															xmlns="http://www.w3.org/2000/svg"
+															fill="none"
+															viewBox="0 0 14 8"
+														>
+															<path
+																stroke="currentColor"
+																stroke-linecap="round"
+																stroke-linejoin="round"
+																stroke-width="1"
+																d="m1 1 5.326 5.7a.909.909 0 0 0 1.348 0L13 1"
+															/>
+														</svg>
+													) : (
+														<svg
+															className="w-[14px] h-[14px] text-gray-800 dark:text-white"
+															aria-hidden="true"
+															xmlns="http://www.w3.org/2000/svg"
+															fill="none"
+															viewBox="0 0 8 14"
+														>
+															<path
+																stroke="currentColor"
+																stroke-linecap="round"
+																stroke-linejoin="round"
+																stroke-width="1"
+																d="m1 13 5.7-5.326a.909.909 0 0 0 0-1.348L1 1"
+															/>
+														</svg>
+													)}
+												</span>
+												<p className="w-full">{subscription.plan}</p>
+											</div>
+										</td>
+										<td className="w-1/5 md:w-1/4 lg:w-1/5 text-center">
+											{subscription.validity}
+										</td>
+										<td className="w-1/5 md:w-1/4 lg:w-1/5  text-center">
+											{subscription.startDate}
+										</td>
+										<td className="w-1/5 md:w-1/4 lg:w-1/5 text-center">
+											{subscription.endDate}
+										</td>
+										<td className="w-1/5 md:w-1/4 lg:w-1/5 text-center p-2">
+											<div className="flex justify-center">
+												<button
+													id="upgrade"
+													// isProcessing={''}
+													// onClick={() => props.registerWithPasskey(true)}
+													className="text-base hover:!bg-primary-800 py-1 px-2 space-x-2 flex font-medium text-white bg-primary-700 rounded-lg hover:bg-primary-800 focus:ring-2 focus:ring-primary-300 sm:w-auto dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 flex justify-center items-center text-center rounded-md"
+												>
+													<svg
+														xmlns="http://www.w3.org/2000/svg"
+														width="17"
+														height="17"
+														viewBox="0 0 17 17"
+														fill="none"
+													>
+														<path
+															d="M8.5 17C3.825 17 0 13.175 0 8.5C0 3.825 3.825 0 8.5 0C13.175 0 17 3.825 17 8.5C17 13.175 13.175 17 8.5 17ZM8.5 1.41667C4.60417 1.41667 1.41667 4.60417 1.41667 8.5C1.41667 12.3958 4.60417 15.5833 8.5 15.5833C12.3958 15.5833 15.5833 12.3958 15.5833 8.5C15.5833 4.60417 12.3958 1.41667 8.5 1.41667ZM9.20833 12.75H7.79167V6.65833L5.45417 8.99583L4.4625 8.00417L8.5 3.96667L12.5375 8.00417L11.5458 8.99583L9.20833 6.65833V12.75Z"
+															fill="white"
+														/>
+													</svg>{' '}
+													<span>Upgrade</span>
+												</button>
+											</div>
+										</td>
+									</tr>
 								);
 							})}
-					</tr>
-				</thead>
-				<tbody className="divide-y w-full">
-					{subscriptionDetails &&
-						subscriptionDetails.map((subscription) => {
-							return (
-								<tr className="bg-white dark:border-gray-700 dark:bg-gray-800 p-2 m-4">
-									<td
-										onClick={() => handleSecondTableClick()}
-										className="whitespace-nowrap font-medium text-gray-900 dark:text-white w-1/5 md:w-1/4 lg:w-1/5 p-2 flex items-center text-center justify-center"
-									>
-										<span>
-											{isSecondTableOpen ? (
-												<svg
-													xmlns="http://www.w3.org/2000/svg"
-													width="12"
-													height="7"
-													viewBox="0 0 12 7"
-													fill="none"
-												>
-													<path
-														d="M10.1643 0.725C10.4786 0.425 10.95 0.425 11.2643 0.725C11.4214 0.875 11.5 1.025 11.5 1.25C11.5 1.475 11.4214 1.625 11.2643 1.775L6.55 6.275C6.23571 6.575 5.76429 6.575 5.45 6.275L0.735714 1.775C0.421429 1.475 0.421429 1.025 0.735714 0.725C1.05 0.425 1.52143 0.425 1.83571 0.725L6 4.7L10.1643 0.725Z"
-														fill="#606972"
-													/>
-												</svg>
-											) : (
-												<svg
-													xmlns="http://www.w3.org/2000/svg"
-													width="6"
-													height="11"
-													viewBox="0 0 6 11"
-													fill="none"
-												>
-													<path
-														d="M0.225 1.33571C-0.0749998 1.02143 -0.0749998 0.55 0.225 0.235715C0.375 0.0785723 0.525 0 0.75 0C0.975 0 1.125 0.0785723 1.275 0.235715L5.775 4.95C6.075 5.26429 6.075 5.73571 5.775 6.05L1.275 10.7643C0.975 11.0786 0.525 11.0786 0.225 10.7643C-0.0749998 10.45 -0.0749998 9.97857 0.225 9.66429L4.2 5.5L0.225 1.33571Z"
-														fill="#606972"
-													/>
-												</svg>
-											)}
-										</span>
-										<span className="pl-2">{subscription.plan}</span>
-									</td>
-									<td className="w-1/5 md:w-1/4 lg:w-1/5 text-center">
-										{subscription.validity}
-									</td>
-									<td className="w-1/5 md:w-1/4 lg:w-1/5  text-center">
-										{subscription.startDate}
-									</td>
-									<td className="w-1/5 md:w-1/4 lg:w-1/5 text-center">
-										{subscription.endDate}
-									</td>
-									<td className="w-1/5 md:w-1/4 lg:w-1/5 text-center">
-										<a
-											href="#"
-											className="font-medium text-cyan-600 hover:underline dark:text-cyan-500 text-center"
-										>
-											Upgrade
-										</a>
-									</td>
+					</tbody>
+				</table>
+				{isSecondTableOpen && (
+					<div className="mx-10">
+						<table className="rounded-none w-full table-auto">
+							<thead className="rounded-none ounded-none bg-gray-200 p-4">
+								<tr>
+									{secondTableHeader &&
+										secondTableHeader.map((header) => {
+											return (
+												<th className="w-1/5 md:w-1/4 lg:w-1/5 text-center m-4 p-2">
+													{header}
+												</th>
+											);
+										})}
 								</tr>
-							);
-						})}
-				</tbody>
-			</table>
-			{isSecondTableOpen && (
-				<div className="mx-10">
+							</thead>
+							<tbody className="divide-y">
+								{data.map((feat) => {
+									return (
+										<tr className="bg-white dark:border-gray-700 dark:bg-gray-800">
+											<td className="whitespace-nowrap font-medium text-gray-900 dark:text-white w-1/5 md:w-1/4 lg:w-1/5  m-4 p-2 text-start">
+												<span className="ml-8">{feat.feature}</span>
+											</td>
+											<td className="w-1/5 md:w-1/4 lg:w-1/5  text-center">
+												{feat.total}
+											</td>
+											<td className="w-1/5 md:w-1/4 lg:w-1/5  text-center">
+												{feat.used}
+											</td>
+											<td className="w-1/5 md:w-1/4 lg:w-1/5  text-center">
+												{feat.remaining}
+											</td>
+											<td className="w-1/5 md:w-1/4 lg:w-1/5 text-center">
+												<div className="flex items-center justify-center text-center w-full">
+													{/* <CircularProgressbar value={feat.precentUsed} /> */}
+													<CircularProgressbar value={50} className="w-6 h-6" />
+												</div>
+											</td>
+										</tr>
+									);
+								})}
+							</tbody>
+						</table>
+					</div>
+				)}
+			</div>
+			<div>
+				<div
+					className="flex items-center justify-between mb-4"
+					id="issued-credentials-list"
+				>
+					<h1 className="ml-1 text-xl font-semibold text-gray-900 sm:text-2xl dark:text-white">
+						Member organizations
+					</h1>
+				</div>
+
+				<div className="overflow-x-auto p-3 mb-4 bg-white dark:bg-gray-500 rounded-sm w-full">
 					<table className="rounded-none w-full table-auto">
 						<thead className="rounded-none ounded-none bg-gray-200 p-4">
 							<tr>
-								{secondTableHeader &&
-									secondTableHeader.map((header) => {
+								{memberHeader &&
+									memberHeader.map((header) => {
 										return (
-											<th className="w-1/5 md:w-1/4 lg:w-1/5 text-center">
-												{header}
+											<th className="w-1/5 md:w-1/4 lg:w-1/5 text-start m-4 p-2">
+												<span className='ml-4'>{header.columnName}</span>
 											</th>
 										);
 									})}
 							</tr>
 						</thead>
 						<tbody className="divide-y">
-							{data.map((feat) => {
+							{memberTableData.map((feat) => {
 								return (
 									<tr className="bg-white dark:border-gray-700 dark:bg-gray-800">
-										<td className="whitespace-nowrap font-medium text-gray-900 dark:text-white w-1/5 md:w-1/4 lg:w-1/5  text-start">
-											<span className="ml-8">{feat.feature}</span>
+										<td className="whitespace-nowrap font-medium text-gray-900 dark:text-white w-1/5 md:w-1/4 lg:w-1/5  m-4 p-2 text-start">
+											<span className="ml-8">{feat.orgName}</span>
 										</td>
-										<td className="w-1/5 md:w-1/4 lg:w-1/5  text-center">
-											{feat.total}
-										</td>
-										<td className="w-1/5 md:w-1/4 lg:w-1/5  text-center">
-											{feat.used}
-										</td>
-										<td className="w-1/5 md:w-1/4 lg:w-1/5  text-center">
-											{feat.remaining}
-										</td>
-										<td className="w-1/5 md:w-1/4 lg:w-1/5  text-center">
-											<a
-												href="#"
-												className="font-medium text-cyan-600 hover:underline dark:text-cyan-500"
-											>
-												{feat.precentUsed}
-											</a>
+										<td className="w-1/5 md:w-1/4 lg:w-1/5 text-start">
+										<span className="ml-6">{feat.planType}</span>
 										</td>
 									</tr>
 								);
@@ -272,7 +272,7 @@ const SubscriptionPlan = () => {
 						</tbody>
 					</table>
 				</div>
-			)}
+			</div>
 		</div>
 	);
 };
